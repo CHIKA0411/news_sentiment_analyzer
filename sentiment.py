b330f3d66b1efddb1e4d1b293100ca202a35a64e
Abha Mahato <121760538+CHIKA0411@users.noreply.github.com>
2025-07-14 03:15:36 +0530

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -2,8 +2,10 @@
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import nltk
-
-nltk.download('vader_lexicon')
+try:
+    nltk.data.find('sentiment/vader_lexicon.zip')
+except LookupError:
+    nltk.download('vader_lexicon')
 
 analyzer = SentimentIntensityAnalyzer()
 
